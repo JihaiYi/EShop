83f3c0852f14d255b1fe7c0c4b9eb700abf7a25d
dahai <yijihai@163.com>
2019-06-17 12:01:05 +0800

    修改默认页面

--- EShop/Controllers/AccountController.cs
+++ EShop/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace EShop.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "确认你的帐户", "请通过单击 <a href=\"" + callbackUrl + "\">這裏</a>来确认你的帐户");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Product");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace EShop.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Product");
         }
 
         //
@@ -449,7 +449,7 @@ namespace EShop.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Product");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
