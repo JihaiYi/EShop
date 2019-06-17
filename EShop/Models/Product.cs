a7d05edd547480db477635edd1e06d48d55da9d5
dahai <yijihai@163.com>
2019-06-17 11:37:43 +0800

    增加product注解，修改prduct的index

--- EShop/Models/Product.cs
+++ EShop/Models/Product.cs
@@ -9,14 +9,20 @@ namespace EShop.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name = "名称") ]
         [Required]
         public string Title { get; set; }
 
+        [Display(Name = "详细信息")]
         public string Description { get; set; }
 
+        
         [Required]
+        [Display(Name = "价格")]
         public decimal  Price { get; set; }
 
+        [Display(Name = "图片")]
         public string ImageUrl { get; set; }
 
 
