a67678ece182ebfdc70145987fa3f42301a55611
dahai <yijihai@163.com>
2019-06-19 11:42:06 +0800

    修改关于和联系方式

--- EShop/Controllers/HomeController.cs
+++ EShop/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace EShop.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "关于";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "联系方式";
 
             return View();
         }
